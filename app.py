829ad48d9439639fa4c071000c42e2c53cb0a50d
Tansi420 <127781271+Tansi420@users.noreply.github.com>
2026-08-20 13:22:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
+from PIL import Image
+
+st.title("Mi Primera App!!")
 
 st.title("HOLA !!! MI NOMBRE xxx")
+st.header("En este espacio comienzo a desarrollar un arma que destruirá el planeta.")
+st.write("bienvenidos a mi web!!")
+image = Image.open('Interfaces Mult2.jpg')
+st.image(image, caption = 'Interface multimodales')
